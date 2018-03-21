7862c9907c6d78c53feeeac4962201aa8acc648e
colin27091 <colinregourd@gmail.com>
2018-03-21 15:36:40 +0100

    ajout fonction resolution_sudoky(sol)

--- sudoku2.py
+++ sudoku2.py
@@ -3,4 +3,12 @@ from copy import*
 from math import sqrt
 import time
 
-je suis perdu
+#E : sol : liste contenant la grille de SUDOKU troué
+#S : affiche dans un canvas de la grille en cours de remplissage
+def resolution_sudoku(sol):
+    p = 0
+    N = int(sqrt(len(sol)+1)) - 1
+    global est_arrive
+    est_arrive = False
+    global sol2
+    placer(p,N,sol)
